eec2f1aa53ef6ee859a365a8c02b4b984278f599
aseelmi-lab <aseel.mashall@gmail.com>
2026-05-11 06:52:22 +0000

    fix: handle input with spaces using strtok

--- shell.c
+++ shell.c
@@ -1,43 +1,6 @@
 #include "main.h"
 
 /**
- * main - Entry point for the simple shell 0.1
- * @ac: Argument count (unused)
- * @av: Argument vector (used for error messages)
- *
- * Return: Always 0
- */
-int main(int ac, char **av)
-{
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	(void)ac;
-
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
-
-		nread = getline(&line, &len, stdin);
-		if (nread == -1) /* Handle Ctrl+D */
-		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			break;
-		}
-
-		if (line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
-
-		if (strlen(line) > 0)
-			execute_command(line, av[0]);
-	}
-	free(line);
-	return (0);
-}
-
-/**
  * execute_command - Forks a process to execute a command
  * @command: The command to execute (full path)
  * @prog_name: Name of the shell program for errors
@@ -69,4 +32,48 @@ void execute_command(char *command, char *prog_name)
 	{
 		wait(&status);
 	}
+}
+
+/**
+ * main - Entry point for the simple shell 0.1
+ * @ac: Argument count (unused)
+ * @av: Argument vector (used for error messages)
+ *
+ * Return: Always 0
+ */
+int main(int ac, char **av)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	char *token;
+	(void)ac;
+
+	while (1)
+	{
+		/* Display prompt only in interactive mode */
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
+
+		nread = getline(&line, &len, stdin);
+		if (nread == -1) /* Handle End of File (Ctrl+D) */
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+
+		/* Tokenize the input to remove spaces, tabs, and newlines */
+		token = strtok(line, " \n\t\r");
+
+		/* Execute only if the command is not empty */
+		if (token != NULL)
+		{
+			if (strcmp(token, "exit") == 0)
+				break;
+			execute_command(token, av[0]);
+		}
+	}
+	free(line);
+	return (0);
 }

92d77be1361b9345d0772831c0dbbbf62df679a8
Jouri-IT <[jouri.f.alsulaiman@gmail.com](mailto:jouri.f.alsulaiman@gmail.com)>
2026-05-22 23:35:44 +0300

    Task 9: replace realloc with malloc+memcpy+free (not in allowed functions)

--- _getline.c
+++ _getline.c
@@ -1,14 +1,12 @@
-#include <unistd.h>
-#include <stdlib.h>
+#include "main.h"
 
 #define BUFFER_SIZE 1024
 
 /**
- * _getline - Reads an entire line from standard input using a static buffer.
- * @lineptr: Pointer to the buffer storing the read line.
- * @n: Pointer to the allocated size of lineptr.
- *
- * Return: Number of characters read (including newline), or -1 on failure/EOF.
+ * _getline - Reads a line from stdin using a static buffer.
+ * @lineptr: Address of pointer to output buffer.
+ * @n: Address of allocated size of *lineptr.
+ * Return: Number of characters read, or -1 on EOF/error.
  */
 ssize_t _getline(char **lineptr, size_t *n)
 {
@@ -17,15 +15,15 @@ ssize_t _getline(char **lineptr, size_t *n)
 	static size_t buffer_len;
 	size_t line_pos = 0;
 	char c;
+	char *new_ptr;
+	size_t new_size;
 
-	/* Validate input pointers */
 	if (lineptr == NULL || n == NULL)
 		return (-1);
 
-	/* If lineptr is empty, allocate an initial buffer */
 	if (*lineptr == NULL || *n == 0)
 	{
-		*n = 128; /* Start with a reasonable initial allocation */
+		*n = 128;
 		*lineptr = malloc(*n);
 		if (*lineptr == NULL)
 			return (-1);
@@ -33,42 +31,38 @@ ssize_t _getline(char **lineptr, size_t *n)
 
 	while (1)
 	{
-		/* If the static buffer is empty, refill it using a single read call */
 		if (buffer_pos >= buffer_len)
 		{
 			buffer_len = read(STDIN_FILENO, buffer, BUFFER_SIZE);
 			buffer_pos = 0;
-
-			/* End of file (EOF) or error occurred */
 			if (buffer_len <= 0)
 			{
 				if (line_pos == 0)
-					return (-1); /* Nothing read at all */
-				break; /* Return what we managed to read so far */
+					return (-1);
+				break;
 			}
 		}
 
-		/* Grab the next character from our static buffer */
 		c = buffer[buffer_pos++];
 
-		/* Make sure lineptr has enough room, reallocate if necessary */
 		if (line_pos >= *n - 1)
 		{
-			*n *= 2;
-			*lineptr = realloc(*lineptr, *n);
-			if (*lineptr == NULL)
+			new_size = *n * 2;
+			new_ptr = malloc(new_size);
+			if (new_ptr == NULL)
 				return (-1);
+			_memcpy(new_ptr, *lineptr, line_pos);
+			free(*lineptr);
+			*lineptr = new_ptr;
+			*n = new_size;
 		}
 
-		/* Store the character */
 		(*lineptr)[line_pos++] = c;
 
-		/* Stop processing if we hit a newline character */
 		if (c == '\n')
 			break;
 	}
 
-	/* Null-terminate the string */
 	(*lineptr)[line_pos] = '\0';
 	return (line_pos);
 }
--- string_helpers.c
+++ string_helpers.c
@@ -60,3 +60,19 @@ char *_strdup(char *str)
 
 	return (dup);
 }
+
+/**
+ * _memcpy - Copies n bytes from src to dest.
+ * @dest: Destination buffer
+ * @src: Source buffer
+ * @n: Number of bytes to copy
+ * Return: Pointer to dest
+ */
+char *_memcpy(char *dest, char *src, size_t n)
+{
+	size_t i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}

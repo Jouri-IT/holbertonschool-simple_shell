d035b8e8444e888a9af049fc873fdd7d6aeae859
Jouri-IT <[jouri.f.alsulaiman@gmail.com](mailto:jouri.f.alsulaiman@gmail.com)>
2026-05-23 18:01:33 +0300

    fix: remove shell_loop prototype, fix builtin_cd include

--- shell.h
+++ shell.h
@@ -14,27 +14,13 @@
 
 extern char **environ;
 
-/* main.c / shell entry */
-void shell_loop(int fd, char **argv);
-
-/* read_file.c */
 char *read_from_file(int fd);
-
-/* path_handler.c */
 char *build_path(char *dir, char *cmd);
 char *get_path(char *command);
-
-/* _getenv.c */
 char *_getenv(const char *name);
-
-/* env_ops.c */
 int _setenv(const char *name, const char *value);
 int _unsetenv(const char *name);
-
-/* builtin_cd.c */
 int builtin_cd(char **args, char **env);
-
-/* string_helpers.c */
 int _strlen(const char *s);
 int _strcmp(char *s1, char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
@@ -45,8 +31,6 @@ char *_strtok(char *str, const char *delim);
 char *_strcpy(char *dest, const char *src);
 int _atoi(const char *s);
 int _is_valid_exit(const char *s);
-
-/* _getline.c */
 ssize_t _getline(char **lineptr, size_t *n);
 
 #endif /* SHELL_H */

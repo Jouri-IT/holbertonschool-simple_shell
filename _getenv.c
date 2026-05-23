1fd7c902d301649ae4e58f9eac9afad665211f3b
Jouri-IT <[jouri.f.alsulaiman@gmail.com](mailto:jouri.f.alsulaiman@gmail.com)>
2026-05-23 18:17:54 +0300

    fix: _getenv.c and env_ops.c include shell.h not main.h

--- _getenv.c
+++ _getenv.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "shell.h"
 
 /**
  * _getenv - gets the value of an environment variable
--- env_ops.c
+++ env_ops.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "shell.h"
 
 static char **custom_env;
 
